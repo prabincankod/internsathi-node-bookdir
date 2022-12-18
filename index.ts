99a161e9d57de492325af9d9321c4b2b3b9a4817
prabincankod <prabinsubedi2016@gmail.com>
2022-12-18 12:36:07 +0545

    :rocket: added cors

--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { prismaClient } from "./prisma/prisma";
 import bookRouter from "./routes/bookRoutes";
+import cors from "cors";
 
 dotenv.config();
 
 const port = process.env.PORT;
 const app: Express = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", async (req: Request, res: Response) => {
   res.status(200).json({ message: "alive" });
